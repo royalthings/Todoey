672fcad6961293801a7b99521f4e8a76e7e536b6
Doombringer <royalthings2019@gmail.com>
2019-03-10 17:27:57 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dima on 3/10/19.
+//  Copyright © 2019 Dima. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
